e64e3e8aec5528e8bb5699d26cb4924b50c98040
Moussa Hafsa <asfahrefajd@gmail.com>
2024-03-25 12:25:30 +0100

    progress3

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 #include "readAnimal.h"
 #include "choices.h"
 #include "read_id.h"
-#define ANIMALSMAX 130
+#define ANIMALSMAX 30
 int main()
 {
     const char *path="/home/hafsa/Animals_list.txt";
     int ch = choices();
-    int id = read_id(); //reading animal's id
     if(ch==1)
     {
+    int id = read_id(); //reading animal's id
     char *content = readAnimal(path,id); //reading an element according to the id
     if(content != NULL)
     {
@@ -24,13 +24,14 @@ int main()
     {
 
     char* animals_arr[ANIMALSMAX];
-    for (int i = 101; i < ANIMALSMAX; i++) //filling the array of articles
+    for (int i = 0; i < ANIMALSMAX; i++) //filling the array of articles
     {
-        animals_arr[i]=readAnimal(path,i+1);
+        animals_arr[i]=readAnimal(path,i+101);
     }
+    printf("\n");
     for(int i=0;i<=ANIMALSMAX;i++) //printing out all elements
     {
-        printf("Animal %d: %s\n", i + 1, animals_arr[i]);
+        printf("Animal:%s\n", animals_arr[i]);
     }
     return 0;
     }
